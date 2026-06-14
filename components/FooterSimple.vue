ebc9610a85986d6cf6257f578a714ad3ab3f9038
giubots <giubots@protonmail.com>
2026-06-14 09:19:28 +0000

    update dependencies, copyright

--- components/FooterSimple.vue
+++ components/FooterSimple.vue
@@ -1,9 +1,7 @@
 <template>
   <footer class="footer footer-center bg-base-300 text-base-content p-4">
     <aside>
-      <p>
-        v1.0 - Copyright © {{ new Date().getFullYear() }} - All rights reserved
-      </p>
+      <p>v1.1 - Copyright © 2025 - All rights reserved</p>
     </aside>
   </footer>
 </template>
